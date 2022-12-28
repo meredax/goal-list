0336be8ca7119c529578d088fd111a1bf17301ee
meredax <mertfberdem@gmail.com>
2022-12-28 14:31:55 +0300

    Added new buttons -Edit buttons -Delete Buttons

--- main.js
+++ main.js
@@ -17,11 +17,38 @@ window.addEventListener('load', () => {
 
        const goal_content_el = document.createElement("div");
        goal_content_el.classList.add("content");
-       goal_content_el.innerText = task;
+       //goal_content_el.innerText = task;       
 
        goal_el.appendChild(goal_content_el);
        
-       list_el.appendChild(goal_el)
+
+       const goal_input_el = document.createElement("input");
+       goal_input_el.classList.add("text");
+       goal_input_el.type = "text"; 
+       goal_input_el.value = task;
+       goal_input_el.setAttribute("readonly","readonly");
+
+
+       goal_content_el.appendChild(goal_input_el);
+
+       const goal_actions_el = document.createElement("div");
+       goal_actions_el.classList.add("actions");
+
+       const goal_edit_el = document.createElement("button");
+       goal_edit_el.classList.add("edit");
+       goal_edit_el.innerHTML = "Edit";
+
+       const goal_delete_el = document.createElement("button");
+       goal_delete_el.classList.add("delete");
+       goal_delete_el.innerHTML = "Delete";
+
+
+       goal_actions_el.appendChild(goal_edit_el);
+       goal_actions_el.appendChild(goal_delete_el);
+
+       goal_el.appendChild(goal_actions_el);
+       list_el.appendChild(goal_el);
+       input.value = "";
 
     })
 
